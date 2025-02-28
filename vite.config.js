f877173f13fe2b3be04952f021bb7d961eb6d141
Denis Chernykh <forgamesandforum@yandex.ru>
2025-02-28 16:04:19 +0500

    Исправить config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-
+  base: process.env.NODE_ENV === 'production' ? '/the-wild-oasis/' : '/',
   resolve: {
     alias: {
       src: '/src',
